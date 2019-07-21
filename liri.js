99ad3b0af76dd3c6722e11a049cbfc94e4615eff
IDGB <49776938+ivergil@users.noreply.github.com>
2019-07-20 19:02:37 -0600

    import all the required NPM packages on my liri.js

--- liri.js
+++ liri.js
@@ -3,4 +3,11 @@ require("dotenv").config();
 // code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
 
+// Import all the required NPM packege.
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+
+// Access to my Spotify keys information
 var spotify = new Spotify(keys.spotify);
