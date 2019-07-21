384a309839a72522b23b35c4aaa0c3e4e1735ad2
IDGB <49776938+ivergil@users.noreply.github.com>
2019-07-20 18:51:12 -0600

    create liri.js

---
+++ liri.js
@@ -0,0 +1,6 @@
+// code to read and set any environment variables with the dotenv package
+require("dotenv").config();
+// code required to import the keys.js file and store it in a variable.
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
